83388cc371353b1c59c61b1bb8f4cc9eff16415b
Robby Grossman <robby@freerobby.com>
2017-03-28 23:39:35 -0400

    Add security comment about removing /execute_command endpoint.

--- web/router.ex
+++ web/router.ex
@@ -24,6 +24,10 @@ defmodule SwarmBrain.Router do
   scope "/api", SwarmBrain do
     pipe_through :api
 
+    # With great power comes great irresponsibility. Remove this prior to public
+    # release. Leaving as is until then for more rapid feature development.
+    # Once use cases are known, move this to a private library and generate
+    # endpoints to invoke specific features.
     post "/execute_command", ShellCommandController, :create
   end
 end
