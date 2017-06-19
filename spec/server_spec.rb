bdd8386cbb5615f7a9caedee62ab1b22cb262c3a
Robby Grossman <robby@freerobby.com>
2017-06-18 20:37:45 -0400

    Update failing test based on server host changes.

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -33,8 +33,7 @@ RSpec.describe AstrolabServer do
       it 'registers the astrolab and returns the response' do
         VCR.use_cassette('heartbeat', :allow_unused_http_interactions => false) do
           post '/api/heartbeat'
-
-          expect(last_response.body).to eq('{"data":{"id":"11","type":"astrolabs","links":{"self":"http://localhost:3000/v1/astrolabs/11"},"attributes":{"last-public-ip-address":"172.18.0.1","last-private-ip-address":"127.0.0.1","last-seen-at":"2017-06-15T02:46:17.555Z","last-country-name":"","last-region-name":"","last-city":"","last-zip-code":"","last-time-zone":"","last-latitude":0.0,"last-longitude":0.0,"created-at":"2017-06-15T02:16:00.016Z","updated-at":"2017-06-15T02:46:17.564Z"}}}')
+          expect(last_response.body).to eq('{"data":{"id":"11","type":"astrolabs","links":{"self":"http://172.19.0.1:3001/v1/astrolabs/11"},"attributes":{"last-public-ip-address":"172.18.0.1","last-private-ip-address":"127.0.0.1","last-seen-at":"2017-06-19T00:33:53.529Z","last-country-name":"","last-region-name":"","last-city":"","last-zip-code":"","last-time-zone":"","last-latitude":0.0,"last-longitude":0.0,"created-at":"2017-06-18T03:21:39.940Z","updated-at":"2017-06-19T00:33:53.533Z"}}}')
         end
       end
     end
