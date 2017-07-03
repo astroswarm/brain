b1543fb7a05bd854328765dc8d6f1b7bdc698623
Robby Grossman <robby@freerobby.com>
2017-07-03 18:27:45 -0400

    Add spec for starting, stopping, and cleaning xapplication.

--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -29,4 +29,64 @@ RSpec.describe Util do
       expect(Util.load_host_data_file 'lan_ip_address').to eql(`cat /host-data/lan_ip_address`)
     end
   end
+
+  describe 'xapplication i/o' do
+    before do
+      @phd2_image = "astroswarm/phd2-x86_64:latest"
+      VCR.turn_off!
+      WebMock.allow_net_connect!
+
+      begin
+        Docker::Image.get @phd2_image
+      rescue Docker::Error::NotFoundError
+        raise "Before running these tests, please run: docker pull #{@phd2_image}"
+      end
+    end
+    after do
+      WebMock.disable_net_connect!
+      VCR.turn_on!
+    end
+
+    def wait_until(&block)
+      started_at = Time.now
+      until !!block.call
+        if Time.now > started_at + 30
+          raise "Condition was not reached in under 30 seconds"
+        else
+          sleep 0.1
+        end
+      end
+    end
+
+    it 'start_xapplication, stop_xapplication, clean_xapplication' do
+      containers = %w(localtunnel websockify xapplication)
+      containers.each do |c|
+        expect(Docker::Container.all(all: true, filters: {name: ["phd2_#{c}_1"]}.to_json).count).to eql(0)
+      end
+
+      Util.start_xapplication @phd2_image
+
+      containers.each do |c|
+        wait_until do
+          Docker::Container.all(all: true, filters: {name: ["phd2_#{c}_1"], status: ['running']}.to_json).count == 1
+        end
+      end
+
+      Util.stop_xapplication @phd2_image
+
+      containers.each do |c|
+        wait_until do
+          Docker::Container.all(all: true, filters: {name: ["phd2_#{c}_1"], status: ['exited']}.to_json).count == 1
+        end
+      end
+
+      Util.clean_xapplication @phd2_image
+
+      containers.each do |c|
+        wait_until do
+          Docker::Container.all(all: true, filters: {name: ["phd2_#{c}_1"]}.to_json).count == 0
+        end
+      end
+    end
+  end
 end
