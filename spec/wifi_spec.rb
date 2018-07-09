d257bb25825235dcdb931a40f4275d1604c67fea
Robby Grossman <robby@freerobby.com>
2018-07-08 21:41:55 -0400

    Add spec for numerical wifi networks.

--- spec/wifi_spec.rb
+++ spec/wifi_spec.rb
@@ -4,6 +4,30 @@ require_relative '../wifi'
 
 RSpec.describe Wifi do
   describe '#add_network and #remove_network integration' do
+    it 'adds and removes numerically-named networks' do
+      File.open(Wifi::WPA_SUPPLICANT_PATH, 'w') {|file| file.truncate 0}
+
+      expect(Wifi.list_networks).to eql([])
+      expect(File.read(Wifi::WPA_SUPPLICANT_PATH)).to eql <<~END
+      END
+
+      Wifi.add_network "1"
+      Wifi.add_network "2"
+      Wifi.add_network "3"
+      Wifi.add_network "4"
+      Wifi.add_network "5"
+
+      Wifi.remove_network "3"
+      expect(Wifi.list_networks).to eql(["1","2","4","5"])
+      Wifi.remove_network "2"
+      expect(Wifi.list_networks).to eql(["1","4","5"])
+      Wifi.remove_network "5"
+      expect(Wifi.list_networks).to eql(["1","4"])
+      Wifi.remove_network "4"
+      expect(Wifi.list_networks).to eql(["1"])
+      Wifi.remove_network "1"
+      expect(Wifi.list_networks).to eql([])
+    end
     it 'adds networks in order, removes them from within file, gracefully handles removal failures' do
       File.open(Wifi::WPA_SUPPLICANT_PATH, 'w') {|file| file.truncate 0}
 
